4f17347c8ef6884e28d71cd2d00c58811560ef7f
maga <fabiomagalhaes890@gmail.com>
2018-07-25 21:53:33 -0300

    testando exceptions

--- src/map.js
+++ src/map.js
@@ -1,9 +1,16 @@
 'use strict'
 
 const map = (arr = [], func = (item) => item) => {
+  if(!Array.isArray(arr))
+    throw new TypeError('this first parameter must be an array')
+
+  if(typeof func !== 'function') {
+    throw new TypeError('the second parameter must be a function')
+  }
+  
   let newArr = []
   arr.map((item) => newArr.push(func(item)))
-  return newArr
+  return newArr 
 }
 
 export default map
--- src/map.test.js
+++ src/map.test.js
@@ -18,3 +18,51 @@ it('map([2,3], (item) => item) should return [2,3]', () => {
 it('map([2,3], (item) => item + 1) should return [3,4]', () => {
   expect(map([2,3], (item) => item + 1)).to.be.deep.equal([3,4])
 })
+
+it('map() should return []', () => {
+  expect(map()).to.be.deep.equal([])
+})
+
+it('map(1) should throw a TypeError', () => {
+  let error
+  try {
+    map(1)
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.name).to.be.equal('TypeError')
+})
+
+it('map(1) should throw an TypeError with message this first parameter must be an array', () => {
+  let error
+  try {
+    map(1)
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.message).to.be.equal('this first parameter must be an array')
+})
+
+it('map([1,2],"function") should throw an TypeError', () => {
+  let error
+  try {
+    map([1,2], '')
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.name).to.be.equal('TypeError')
+})
+
+it('map([1,2],"function") should throw an TypeError an message the second parameter must be a function', () => {
+  let error
+  try {
+    map([1,2], '')
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.message).to.be.equal('the second parameter must be a function')
+})
